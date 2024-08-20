b9063a21962feba390f99e34fead1e77d994b76d
Arif DesktopMode <achadzainul67@gmail,con>
2024-08-20 17:23:31 +0700

    Update the game suwit jawa v2.0

--- G-GameSuwitv2.0/main.js
+++ G-GameSuwitv2.0/main.js
@@ -1,30 +1,113 @@
-let tanya = true;
-while(tanya) {
-    // menangkap pilihan player
-    let p = prompt('pilih : gajah, semut, orang');
-
-    // menangkap pilihan computer
-    // membangkitkan bilangan random
-    let comp = Math.random();
-    if(comp < 0.34) {
-        comp = 'gajah';
-    }else if(comp >= 0.34 && comp < 0.67) {
-        comp = 'orang';
-    }else {
-        comp = 'semut';
-    }
-
-    // menentukan rules
-    let hasil = '';
-    if(p == comp) {
-        hasil = 'seri';
-    }else if(p == 'gajah') {
-        hasil = (comp == 'orang') ? 'Menang' : 'Kalah';
-    }else if (p == 'orang') {
-        hasil = (comp == 'semut') ? 'Kalah' : 'Menang';
-    }else if(p == 'semut') {
-        hasil = (comp == 'orang') ?  'Kalah' : 'Menang';
-    }else {
-        hasil = 'Memasukkan pilihan yang salah!';
-    }
+function getPilihanComputer() {
+    const comp = Math.random();
+    if(comp < 0.34) return 'gajah';
+    else if(comp >= 0.34 && comp < 0.67) return 'orang';
+    return 'semut';
 }
+
+function getResult(comp, player) {
+    if (player == comp) return 'Seri🤝';
+    if (player == 'gajah') return (comp == 'orang') ? 'Menang🎉' : 'Kalah😭';
+    if (player == 'orang') return (comp == 'semut') ? 'Kalah😭' : 'Menang🎉';
+    if (player == 'semut') return (comp == 'orang') ?  'Kalah😭' : 'Menang🎉';
+}
+
+const imgComp = document.querySelector(".comp");
+const info = document.querySelector(".info");
+
+function putar() {
+    const gambar = ['gajah', 'semut', 'orang'];
+    let i = 0;
+    const wMulai = new Date().getTime();
+    setInterval(
+        function(){
+            if(new Date().getTime() - wMulai > 1000) {
+                clearInterval 
+                return 0;
+            }
+            imgComp.setAttribute('src', `img/${gambar[i++]}.png`);
+            if(i == gambar.length)  i = 0;
+        }
+    ), 100
+}
+
+const pilihan = document.querySelectorAll('.pilihan img')
+pilihan.forEach(function(pil) {
+    pil.addEventListener('click', function() {
+        console.log(pil);
+        const pilihanComputer = getPilihanComputer();
+        const pilihanPlayer = pil.className;
+        const result = getResult(pilihanComputer, pilihanPlayer);
+        
+        putar();
+
+        setTimeout(
+            function() {
+                imgComp.setAttribute('src', `img/${pilihanComputer}.png`);
+                info.innerHTML = result;
+            }, 1000
+        );
+        
+        
+        console.log(`Kopumter: ${pilihanComputer}`);
+        console.log(`Player: ${pilihanPlayer}`);
+        console.log(`Hasil ${result}`);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// useGajah.addEventListener('click', function() {
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = useGajah.className;
+//     const result = getResult(pilihanComputer, pilihanPlayer);
+
+//     imgComp.setAttribute('src', `img/${pilihanComputer}.png`);
+//     info.innerHTML = result;
+
+//     console.log(`Kopumter: ${pilihanComputer}`);
+//     console.log(`Player: ${pilihanPlayer}`);
+//     console.log(`Hasil ${result}`)
+// })
+// useOrang.addEventListener('click', function() {
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = useOrang.className;
+//     const result = getResult(pilihanComputer, pilihanPlayer);
+
+//     imgComp.setAttribute('src', `img/${pilihanComputer}.png`);
+//     info.innerHTML = result;
+
+//     console.log(`Komputer: ${pilihanComputer}`);
+//     console.log(`Player: ${pilihanPlayer}`);
+//     console.log(`Hasil ${result}`)
+// })
+// useSemut.addEventListener('click', function() {
+//     const pilihanComputer = getPilihanComputer();
+//     const pilihanPlayer = useSemut.className;
+//     const result = getResult(pilihanComputer, pilihanPlayer);
+
+//     imgComp.setAttribute('src', `img/${pilihanComputer}.png`);
+//     info.innerHTML = result;
+
+//     console.log(`Komputer: ${pilihanComputer}`);
+//     console.log(`Player: ${pilihanPlayer}`);
+//     console.log(`Hasil ${result}`)
+// })
