ac238d50cf3d92d290d3597adb381e95942dcf2f
ArifDev-coder <achadzainul67@gmail.com>
2024-08-22 22:41:28 +0700

    edit

--- I-IMAGE_GALLERY/index.js
+++ I-IMAGE_GALLERY/index.js
@@ -1,9 +1,18 @@
 const thumbs = document.querySelectorAll('.thumb');
 const jumbo = document.querySelector('.jumbo');
 thumbs.forEach(function(img) {
-    img.addEventListener('click', function() {
+    img.addEventListener('click', function(e) {
         let src = img.getAttribute('src');
         jumbo.setAttribute('src', src);
-        console.log(src);
-    })
+        jumbo.classList.add('fade');
+        setTimeout(function() {
+            jumbo.classList.remove('fade')
+        }, 500);
+        
+        thumbs.className = 'thumb'
+        
+        e.target.classList.add('active');
+            e.preventDefault();
+            // console.log(src);
+        })
 })
